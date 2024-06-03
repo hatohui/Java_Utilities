9acda3374a9a9d58c54a81682757b9a8bbd78615
Lê Sỹ Tuyền <130513663+Hatohui@users.noreply.github.com>
2024-06-03 18:53:15 +0700

    Display stuffs for Java
    yems

---
+++ Display.java
@@ -0,0 +1,75 @@
+package utils;
+
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class Display {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    /**
+     * Generates a menu of options.
+     * <p>
+     * Examples: for input <b>["foo","boo","woo"]</b>
+     *  <br>1. foo
+     *  <br>2. boo
+     *  <br>3. woo
+     *  <br>> Option: [your input]
+     * @param items takes in an array of Strings that will be
+     *              printed as a list of options
+     * @return an integer ranging from 1 to the list's length that
+     * indicates the option.
+     * */
+    public static int getOptions(String[] items) {
+        int no = 1;
+        for (String item : items) {
+            System.out.println("\t" + no + "." + item);
+            no++;
+        }
+        while (true) {
+            try {
+                System.out.print("> Option: ");
+                int option = Integer.parseInt(scanner.nextLine());
+                if (option > (no - 1) || option < 1)
+                    throw new IllegalArgumentException("Please pick a proper option. (1-" + (no-1) + ")");
+                return option;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * Generates a loading bar: █████░░░░░░
+     * <br>(change the source code if you want a different bar)
+     * @param message - a string to display before the loading bar.
+     * @param loadTime - an integer indicates amount of tick
+     *                 for the loading bar.
+     * @param interval - an integer that indicatses time period
+     *                per tick (in milliseconds).
+     * */
+    public static void loadingBar(String message, int loadTime, int interval) {
+        try {
+            //setups
+            char incomplete = '░';
+            char completed = '█';
+            StringBuilder builder = new StringBuilder();
+
+            if (!message.isEmpty()) message += "\t";
+
+            //generates the stream
+            Stream.generate(() -> incomplete)
+                    .limit(loadTime)
+                    .forEach(builder::append);
+
+            //loads
+            for (int i = 0; i < loadTime; ++i) {
+                builder.replace(i,i+1, java.lang.String.valueOf(completed));
+                java.lang.String loading = "\r"+ message + builder;
+                System.out.print(loading);
+                Thread.sleep(interval);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
